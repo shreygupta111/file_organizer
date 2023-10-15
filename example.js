ac910a7e408b2030ca0884e403595d97e8d713f9
shrey <shreygupta111@gmail.com>
2023-10-15 18:39:21 -0400

    Code tested locally

---
+++ example.js
@@ -0,0 +1,52 @@
+const path = require("path")
+const util = require("util")
+const fs = require("fs")
+// const { config } = require("process")
+const { dir } = require("console")
+
+const readdir = util.promisify(fs.readdir )
+
+const config = {
+    typeDirs: [
+        {type: ".pdf", directory: "documents"},
+        {type: ".png", directory: "images"},
+        {type: ".mp3", directory: "music"},
+    ]
+}
+
+
+const directory = process.argv[2]
+// const directory = "example"
+if(!directory){
+    console.log("Specify the target directory")
+}
+
+[...config.typeDirs, {directory: "other"}].map(d => {
+    const dirname = `${directory}/${d.directory}`
+    if(!fs.existsSync(dirname)){
+        fs.mkdirSync(dirname)
+    }
+});
+
+(async ()=>{
+    const files = await readdir(directory)
+
+    files.forEach(file => {
+        const extname = path.extname(file)
+        if(!extname){
+            return
+        }
+
+        const {directory: targetDir = "other"} = config.typeDirs.find(dir => dir.type == extname) ||{}
+
+        const oldPath = path.join(__dirname, directory, file)
+        const newPath = path.join(__dirname, directory, targetDir, file)
+
+        fs.rename(oldPath, newPath, function(err){
+            if(err){
+                throw err
+            }
+        })
+    })
+
+})()
